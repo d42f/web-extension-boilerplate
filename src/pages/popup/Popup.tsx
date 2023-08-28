6536a3e5b1161170d5da43b858939973cf87684d
d42f <dmitri.frolof@gmail.com>
2023-08-28 22:43:12 +0200

    refactored styles and added store examples

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,16 +1,25 @@
+import { useEffect } from 'react';
+
 import Logo from '@assets/react.svg';
 import { Button } from '@components/Button';
-import { toggle } from '@store/toolbar';
+import { setVisible } from '@store/toolbar';
 import styles from './Popup.module.scss';
 
 export const Popup = (): JSX.Element => {
+  const handleVisibleClick = () => {
+    setVisible(true);
+    window.close();
+  };
+
+  useEffect(() => {
+    setVisible(false);
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <Logo />
-      <Button size="small" variant="filled" theme="secondary" onClick={() => toggle()}>
-        Toggle content component
-      </Button>
       Popup component
+      <Button onClick={handleVisibleClick}>Show content component</Button>
     </div>
   );
 };
--- src/store/toolbar.ts
+++ src/store/toolbar.ts
@@ -1,4 +1,4 @@
-import { action } from 'nanostores';
+import { action, WritableAtom } from 'nanostores';
 import { Toolbar } from '@src/models/Toolbar';
 import { persistentAtom } from './persistentStore';
 
@@ -6,11 +6,15 @@ export const $toolbar = persistentAtom<Toolbar>('toolbar', {
   visible: false,
 });
 
-export const toggle = action($toolbar, 'toggle', store => {
-  const value = store.get();
+export const setVisible = action($toolbar, 'setVisible', (store: WritableAtom<Toolbar | undefined>, value: boolean) => {
   store.set({
-    ...value,
-    visible: !value?.visible,
+    ...store.get(),
+    visible: value,
   });
   return store.get();
+});
+
+export const toggle = action($toolbar, 'toggle', (store: WritableAtom<Toolbar | undefined>) => {
+  const state = store.get();
+  return setVisible(!state?.visible);
 });
